9dc92c02ecde5cd4ebfff4092bc7855639720f46
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2021-03-11 22:25:23 +0500

    Move tabs data to json

--- js/index.js
+++ js/index.js
@@ -1,53 +1,39 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const data = [
-    {
-      name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
-      img: 'img/iPhone-graphite.png',
-      memory: 128,
-      price: 99_990,
-    },
-    {
-      name: 'Смартфон Apple iPhone 12 Pro 256GB Silver',
-      img: 'img/iPhone-silver.png',
-      memory: 256,
-      price: 109_990,
-    },
-    {
-      name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
-      img: 'img/iPhone-blue.png',
-      memory: 128,
-      price: 99_990,
-    },
-  ]
 
-  const cardDetailChangeList = document.querySelectorAll('.card-details__change')
-  const cardTitleElement = document.querySelector('.card-details__title')
-  const cardImageElement = document.querySelector('.card__image_item')
-  const cardPriceElement = document.querySelector('.card-details__price')
-  const cardMemoryElement = document.querySelector('.description__memory-value')
-
-
-  const deactivateTabs = () => {
-    cardDetailChangeList.forEach( btn => {
-      btn.classList.remove('active')
-    } )
+  const tabs = data => {
+    const cardDetailChangeList = document.querySelectorAll('.card-details__change')
+    const cardTitleElement = document.querySelector('.card-details__title')
+    const cardImageElement = document.querySelector('.card__image_item')
+    const cardPriceElement = document.querySelector('.card-details__price')
+    const cardMemoryElement = document.querySelector('.description__memory-value')
+  
+  
+    const deactivateTabs = () => {
+      cardDetailChangeList.forEach( btn => {
+        btn.classList.remove('active')
+      } )
+    }
+  
+    cardDetailChangeList.forEach( (btn, index) => {
+      btn.addEventListener('click', event => {
+        event.preventDefault()
+  
+        deactivateTabs()
+        btn.classList.add('active')
+  
+        cardTitleElement.textContent = data[index].name
+        cardImageElement.src = data[index].img
+        cardImageElement.alt = data[index].name
+        cardPriceElement.textContent = data[index].price.toLocaleString() + '₽'
+        cardMemoryElement.textContent = data[index].memory
+      })
+    })
   }
 
-  cardDetailChangeList.forEach( (btn, index) => {
-    btn.addEventListener('click', event => {
-      event.preventDefault()
-
-      deactivateTabs()
-      btn.classList.add('active')
-
-      cardTitleElement.textContent = data[index].name
-      cardImageElement.src = data[index].img
-      cardImageElement.alt = data[index].name
-      cardPriceElement.textContent = data[index].price.toLocaleString() + '₽'
-      cardMemoryElement.textContent = data[index].memory
-    })
-  })
-
+  fetch('tabs.json')
+    .then(response => response.json())
+    .then(json => tabs(json))
+    .catch(error => alert('Произошла ошибка: ' + error.toString()))
 })
